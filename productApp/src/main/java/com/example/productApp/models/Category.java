7e42361787aa3e508e7772232f4f66fbf1a5ddf4
Omar Elsawi <omarelsawi98@gmail.com>
2023-05-12 19:07:54 +0300

    create category model

--- productApp/src/main/java/com/example/productApp/models/Category.java
+++ productApp/src/main/java/com/example/productApp/models/Category.java
@@ -1,4 +1,26 @@
-package main.java.com.example.productApp.models;
+package com.example.productApp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Category {
+    @Id
+    @Column(name="id")
+    private int id;
+
+    @Column(name="category")
+    private String category;
+
 }
