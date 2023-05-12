795105ac7f77da609b2c63fdf65ed0c5934bec19
Anas Ayman <51910913+Anas36@users.noreply.github.com>
2023-05-13 00:01:23 +0300

    Category

--- productApp/src/main/java/com/example/productApp/controllers/CategoryController.java
+++ productApp/src/main/java/com/example/productApp/controllers/CategoryController.java
@@ -3,15 +3,24 @@ package com.example.productApp.controllers;
 import com.example.productApp.models.Category;
 import com.example.productApp.services.CategoryService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@AllArgsConstructor
+@RestController
+@RequestMapping("/category")
 public class CategoryController {
 
     private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
     @GetMapping("/getCategories")
     List<Category> getAllCategories(){
