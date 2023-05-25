e279e7ee9a61e90ef398132cfcfb477bfd6aa248
ayadi99 <elayadi@ayadi.dev>
2023-05-25 19:48:01 +0300

    deleted old userApp from main app

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -51,7 +51,7 @@ import org.springframework.http.ResponseEntity;
 
 // @EnableScheduling
 // @EnableRabbit
-@SpringBootApplication()
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 @EnableMongoRepositories
 public class DemoApplication {
